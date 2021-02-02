1e4521ed9348e39378510547f8f057eade54c7e2
Karol Furmanski <furmanskikarol@gmail.com>
2021-02-02 23:05:54 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Decerto-Task
---
+++ src/main/java/org/furmanski/DecertoTaskApplication.java
@@ -0,0 +1,13 @@
+package org.furmanski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecertoTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecertoTaskApplication.class, args);
+    }
+
+}
